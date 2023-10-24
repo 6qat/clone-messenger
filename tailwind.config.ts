1564ce8ea2f0f0602d21c3c7289e643fdc09bef9
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-10-24 13:44:13 -0300

    Playing with form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,8 @@ const config: Config = {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/forms')({
+        strategy: 'class',
+    })],
 };
 export default config;
