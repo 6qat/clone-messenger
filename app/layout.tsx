b370e7f4c9c4c88627fe9d04631e6001ecd17cf0
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-10-30 16:30:40 -0300

    Add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type {Metadata} from 'next';
-import {Inter} from 'next/font/google';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import React from 'react';
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Messenger Clone',
-    description: 'Messenger Clone',
+  title: 'Messenger Clone',
+  description: 'Messenger Clone',
 };
 
 export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode
+                                     children,
+                                   }: {
+  children: React.ReactNode
 }) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang='en'>
+    <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
