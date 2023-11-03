76a7c0f19f2e6d2bc376ee620bf867ac97fe378a
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-11-03 16:22:35 -0300

    Logging in using email and password.

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,8 @@ import Button from '@/app/components/Button';
 import AuthSocialButton from '@/app/(site)/components/AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 import axios from 'axios';
+import toast from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -39,10 +41,32 @@ export default function AuthForm() {
 
     if (variant === 'REGISTER') {
       // Axios Register
-      axios.post('/api/register', data);
+      axios
+        .post('/api/register', data)
+        .catch(() => {
+          toast.error("Something went wrong. We couldn't create your account.");
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     }
     if (variant === 'LOGIN') {
       // NextAuth SignIn
+      signIn('credentials', {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error('Invalid credentials');
+          }
+          if (callback?.ok && !callback?.error) {
+            toast.success('Welcome back!');
+          }
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     }
   };
 
